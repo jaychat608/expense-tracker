ce9395884053d610258044feb4def563ec9561a7
jaychat608 <jayantachatterjee076@gmail.com>
2025-04-23 03:21:19 -0600

    setting up endpoints, add/update/delete records

--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -17,4 +17,54 @@ router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
     }
 })
 
+router.post("/", async (req: Request, res: Response) => {
+    try{
+        const newRecordBody = req.body
+        const newRecord = new financialRecordModel(newRecordBody);
+        const savedRecord = await newRecord.save();
+
+        res.status(200).send(savedRecord);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+    try{
+        const id = req.params.id;
+        const newRecordBody = req.body
+        const record = await financialRecordModel.findByIdAndUpdate(id, newRecordBody, {new: true});
+
+        if (!record) {
+            res.status(404).send()
+            return;
+        };
+
+        res.status(200).send(record);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try{
+        const id = req.params.id;
+        const record = await financialRecordModel.findByIdAndDelete(id);
+
+        if (!record) {
+            res.status(404).send()
+            return;
+        };
+
+        res.status(200).send(record);
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+
 export default router;
