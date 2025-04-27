3d13bbba78a47e892ee264ab39cac0469540d466
jaychat608 <jayantachatterjee076@gmail.com>
2025-04-27 07:05:10 -0600

    made sure data was being passed into mongoDB

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -24,7 +24,10 @@ export const FinancialRecordsProvider = ({children} : {children: React.ReactNode
     const [records, setRecords] = useState<FinancialRecord[]>([]);
 
     const addRecord = async (record: FinancialRecord) => {
-        const response = await fetch("http://localhost:3001/financial-records", {method: "POST", body: JSON.stringify(record)});
+        const response = await fetch("http://localhost:3001/financial-records", {
+            method: "POST", 
+            body: JSON.stringify(record), 
+            headers: {'Content-Type': "application/json"}});
         try{
             if (response.ok) {
                 const newRecord = await response.json()
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,13 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from './routes/financial-records';
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 const mongoURI: string = "mongodb+srv://jayantachatterjee076:EnK44ERLEeKWjNv2@expense-tracker.jmqndca.mongodb.net/";
 
