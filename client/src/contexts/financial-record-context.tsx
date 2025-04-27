e5ea7f55997b4419b8f9d1c12288c5beecdff83d
jaychat608 <jayantachatterjee076@gmail.com>
2025-04-27 07:40:33 -0600

    added fetch records function to make sure correct user data is retrieved upon login.

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -1,4 +1,5 @@
-import {createContext, useContext, useState} from "react";
+import { useUser } from "@clerk/clerk-react";
+import {createContext, useContext, useState, useEffect} from "react";
 
 
 interface FinancialRecord {
@@ -22,6 +23,22 @@ export const FinancialRecordsContext = createContext<FinancialRecordsContextType
 export const FinancialRecordsProvider = ({children} : {children: React.ReactNode}) => {
 
     const [records, setRecords] = useState<FinancialRecord[]>([]);
+    const { user } = useUser();
+
+    const fetchRecords = async () => {
+        if (!user){
+            return;
+        }
+        const response = await fetch(`http://localhost:3001/financial-records/getAllByUserID/${user?.id}`)
+        if (response.ok){
+            const records = await response.json();
+            setRecords(records);
+        }
+    }
+
+    useEffect(() => {
+        fetchRecords;
+    }, [user]);
 
     const addRecord = async (record: FinancialRecord) => {
         const response = await fetch("http://localhost:3001/financial-records", {
